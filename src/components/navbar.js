b926713e50bc2c0ffc49c075166354b89b2b406c
Fidel C <coronel08@aol.com>
2021-12-02 22:32:27 -0800

    added pricing section a lot of css, and some html

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,16 @@
 const navbar = () => {
     let template = `
-    <p class="text-white md:m-12 text-center mt-4">
-        Logo
-    </p>
+    <a href="/" class="text-white md:m-12 text-center mt-4">
+        Home
+    </a>
     <div class="hidden md:flex gap-8 text-blue-200">
         <p class="">Test</p>
         <p class="">Test2</p>
-        <p class="">Test3</p>
     </div>
     <div class="flex gap-8 mr-8 m-4 text-blue-200 justify-between">
-        <p>icon</p>
-        <p>icon</p>
-        <p>icon</p>
+        <a href="#wrk">Work</a>
+        <a href="#prjcts">Projects</a>
+        <a href="#pricing">Pricing</a>
     </div>
     `
 
