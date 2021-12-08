8a94559df2d766f0079e10ff923dcd301de76a4c
Fidel C <coronel08@aol.com>
2021-12-08 12:29:48 -0800

    wrote animate on scroll from scratch

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -3,8 +3,8 @@ const progressBar = () => {
     const progressBar = document.querySelector('.progress-bar')
     const scrollToTop = document.querySelector('.scroll-to-top')
 
-    function stretch(){
-        const pixelScrolled  = window.scrollY
+    function stretch() {
+        const pixelScrolled = window.scrollY
         const viewPortHeight = window.innerHeight
         const totalHeightScrollable = body.scrollHeight
 
@@ -13,11 +13,26 @@ const progressBar = () => {
         progressBar.style.width = Math.round(pixelsToPercentage) + "%"
 
         // Scroll to top button only visible at certain point
-        if(pixelScrolled >= 500){
+        if (pixelScrolled >= 500) {
             scrollToTop.style.visibility = "visible"
         } else {
             scrollToTop.style.visibility = "hidden"
         }
+
+        // Title animate on scroll from scratch
+        /*
+        const title = document.querySelector("#prjcts")
+        let rect = title.getBoundingClientRect()
+        let windowHeight = window.innerHeight
+        let visibility = 150
+        if (rect.top >= windowHeight - visibility) {
+            title.setAttribute('style', 'opacity: 0; transform: translateY(150%)');
+            title.classList.add("fade-right")
+        } else {
+            title.setAttribute('style', 'opacity: 1; transform: translateY(0%)');
+        }
+        */
+
     }
 
     window.addEventListener('scroll', stretch)
